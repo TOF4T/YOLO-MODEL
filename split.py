321fc8604863fe2fbce7fc6f4613222dbfe1ee61
TOF4T <lethaomht@gmail.com>
2026-05-16 01:25:59 +0700

    Update split.py

--- split.py
+++ split.py
@@ -69,7 +69,8 @@ def split_dataset(root_dir, output_dir):
     print("\n=== THỐNG KÊ SỐ LƯỢNG NHÃN BAN ĐẦU ===")
     for cls, count in sorted(class_counts.items()):
         status = "🟢 >=100 (7:2:1)" if count >= 100 else "🔴 <100 (4:3:3)"
-        print(f"Nhãn [{int(cls) + 1}]: {count} | {status}")
+        label_id = int(cls) + 1
+        print(f"Nhãn [{label_id}]: {count} | {status}")
 
     grouped_files = {}
     for f in all_files:
